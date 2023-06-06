2b4c9a9259f63c16a180603ea4be5b476e41a10d
Simon Porritt <simon@jsplumbtoolkit.com>
2023-06-06 22:03:16 +1000

    - add text color to div style - remove edit button

--- apps/flowchart-builder/vue3/src/components/Node.vue
+++ apps/flowchart-builder/vue3/src/components/Node.vue
@@ -9,14 +9,13 @@
 
 </script>
 <template>
-    <div :style="{width:obj.width + 'px', height:obj.height + 'px'}" class="flowchart-object" data-jtk-target="true" data-jtk-target-port-type="target">
+    <div :style="{width:obj.width + 'px', height:obj.height + 'px', color:obj.textColor}" class="flowchart-object" data-jtk-target="true" data-jtk-target-port-type="target">
         <span>{{obj.text}}</span>
-        <jsplumb-shape :obj="obj" :width="obj.width" :height="obj.height" :shape-library="shapeLibrary"></jsplumb-shape>
+        <jsplumb-shape :obj="obj" :shape-library="shapeLibrary"></jsplumb-shape>
         <div class="jtk-connect" data-jtk-anchor-x="0" data-jtk-anchor-y="0.5" data-jtk-orientation-x="-1"  data-jtk-orientation-y="0" data-jtk-source="true" data-jtk-port-type="source" style="left:0;top:50%;transform: translate(-50%, -50%);"></div>
         <div class="jtk-connect" data-jtk-anchor-x="1" data-jtk-anchor-y="0.5" data-jtk-orientation-x="1"  data-jtk-orientation-y="0" data-jtk-source="true" data-jtk-port-type="source" style="right:0;top:50%;transform: translate(50%, -50%);"></div>
         <div class="jtk-connect" data-jtk-anchor-x="0.5" data-jtk-anchor-y="0" data-jtk-orientation-x="0"  data-jtk-orientation-y="-1" data-jtk-source="true" data-jtk-port-type="source" style="left:50%;top:0;transform: translate(-50%, -50%);"></div>
         <div class="jtk-connect" data-jtk-anchor-x="0.5" data-jtk-anchor-y="1" data-jtk-orientation-x="0"  data-jtk-orientation-y="1" data-jtk-source="true" data-jtk-port-type="source" style="left:50%;bottom:0;transform: translate(-50%, 50%);"></div>
-        <div class="node-edit node-action"></div>
         <div class="node-delete node-action delete"></div>
     </div>
 </template>
