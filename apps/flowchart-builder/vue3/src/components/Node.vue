9889a9a3b5b3d2057c7409a5e727a2fba4e55dbf
Simon Porritt <simon@jsplumbtoolkit.com>
2023-06-09 11:02:18 +1000

    use CSS styles for connect elements rather than inline style

--- apps/flowchart-builder/vue3/src/components/Node.vue
+++ apps/flowchart-builder/vue3/src/components/Node.vue
@@ -12,10 +12,10 @@
     <div :style="{width:obj.width + 'px', height:obj.height + 'px', color:obj.textColor}" class="flowchart-object" data-jtk-target="true" data-jtk-target-port-type="target">
         <span>{{obj.text}}</span>
         <jsplumb-shape :obj="obj" :shape-library="shapeLibrary"></jsplumb-shape>
-        <div class="jtk-connect" data-jtk-anchor-x="0" data-jtk-anchor-y="0.5" data-jtk-orientation-x="-1"  data-jtk-orientation-y="0" data-jtk-source="true" data-jtk-port-type="source" style="left:0;top:50%;transform: translate(-50%, -50%);"></div>
-        <div class="jtk-connect" data-jtk-anchor-x="1" data-jtk-anchor-y="0.5" data-jtk-orientation-x="1"  data-jtk-orientation-y="0" data-jtk-source="true" data-jtk-port-type="source" style="right:0;top:50%;transform: translate(50%, -50%);"></div>
-        <div class="jtk-connect" data-jtk-anchor-x="0.5" data-jtk-anchor-y="0" data-jtk-orientation-x="0"  data-jtk-orientation-y="-1" data-jtk-source="true" data-jtk-port-type="source" style="left:50%;top:0;transform: translate(-50%, -50%);"></div>
-        <div class="jtk-connect" data-jtk-anchor-x="0.5" data-jtk-anchor-y="1" data-jtk-orientation-x="0"  data-jtk-orientation-y="1" data-jtk-source="true" data-jtk-port-type="source" style="left:50%;bottom:0;transform: translate(-50%, 50%);"></div>
+        <div class="jtk-connect jtk-connect-left" data-jtk-anchor-x="0" data-jtk-anchor-y="0.5" data-jtk-orientation-x="-1"  data-jtk-orientation-y="0" data-jtk-source="true" data-jtk-port-type="source"></div>
+        <div class="jtk-connect jtk-connect-right" data-jtk-anchor-x="1" data-jtk-anchor-y="0.5" data-jtk-orientation-x="1"  data-jtk-orientation-y="0" data-jtk-source="true" data-jtk-port-type="source"></div>
+        <div class="jtk-connect jtk-connect-top" data-jtk-anchor-x="0.5" data-jtk-anchor-y="0" data-jtk-orientation-x="0"  data-jtk-orientation-y="-1" data-jtk-source="true" data-jtk-port-type="source"></div>
+        <div class="jtk-connect jtk-connect-bottom" data-jtk-anchor-x="0.5" data-jtk-anchor-y="1" data-jtk-orientation-x="0"  data-jtk-orientation-y="1" data-jtk-source="true" data-jtk-port-type="source"></div>
         <div class="node-delete node-action delete"></div>
     </div>
 </template>
