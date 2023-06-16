96d1a886a792aeba7e5dd31a5de53f47245a5e3c
Simon Porritt <simon@jsplumbtoolkit.com>
2023-06-16 10:57:35 +1000

    use anchorPositions array to draw connection sources

--- apps/flowchart-builder/angular/src/app/node.component.ts
+++ apps/flowchart-builder/angular/src/app/node.component.ts
@@ -1,15 +1,24 @@
 import {Component} from "@angular/core"
 import {BaseNodeComponent} from "@jsplumbtoolkit/browser-ui-angular"
 
+import { anchorPositions } from "./app.component"
+
 @Component({
     template:`<div style="width:{{obj.width}}px;height:{{obj.height}}px;color:{{obj.textColor}}" class="flowchart-object" data-jtk-target="true" data-jtk-target-port-type="target">
         <span>{{obj.text}}</span>
         <jtk-shape [obj]="obj" [width]="obj.width" [height]="obj.height"></jtk-shape>
-        <div class="jtk-connect jtk-connect-left" data-jtk-anchor-x="0" data-jtk-anchor-y="0.5" data-jtk-orientation-x="-1"  data-jtk-orientation-y="0" data-jtk-source="true" data-jtk-port-type="source"></div>
-        <div class="jtk-connect jtk-connect-right" data-jtk-anchor-x="1" data-jtk-anchor-y="0.5" data-jtk-orientation-x="1"  data-jtk-orientation-y="0" data-jtk-source="true" data-jtk-port-type="source"></div>
-        <div class="jtk-connect jtk-connect-top" data-jtk-anchor-x="0.5" data-jtk-anchor-y="0" data-jtk-orientation-x="0"  data-jtk-orientation-y="-1" data-jtk-source="true" data-jtk-port-type="source"></div>
-        <div class="jtk-connect jtk-connect-bottom" data-jtk-anchor-x="0.5" data-jtk-anchor-y="1" data-jtk-orientation-x="0"  data-jtk-orientation-y="1" data-jtk-source="true" data-jtk-port-type="source"></div>
+
+        <div *ngFor="let anchor of anchorPositions" 
+             class="jtk-connect jtk-connect-{{anchor.id}}" 
+             [attr.data-jtk-anchor-x]="anchor.x" 
+             [attr.data-jtk-anchor-y]="anchor.y" 
+             [attr.data-jtk-orientation-x]="anchor.ox" 
+             [attr.data-jtk-orientation-y]="anchor.oy" 
+             data-jtk-source="true"></div>
+        
         <div class="node-delete node-action delete" (click)="this.removeNode()"></div>
     </div>`
 })
-export class NodeComponent extends BaseNodeComponent { }
+export class NodeComponent extends BaseNodeComponent {
+    anchorPositions = anchorPositions
+}
