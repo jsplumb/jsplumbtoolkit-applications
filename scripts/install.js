1f2b7e044322b4d6c4d181c13ca92ae3d7560c43
Simon Porritt <simon@jsplumbtoolkit.com>
2023-06-16 10:57:13 +1000

    support cmd line args. build all libs if none specified on command line.

--- scripts/install.js
+++ scripts/install.js
@@ -1,25 +1,35 @@
 const g = require('./gatlight')
 const c = require('child_process')
+const p = require('../package.json')
 
-function doInstall(lib) {
+function install(app, lib) {
+    const appDir = `./apps/${app}`,
+        libDir = `${appDir}/${lib}`
 
-    g.ls("./apps").forEach(d => {
-        const appDir = `./apps/${d}`,
-            libDir = `${appDir}/${lib}`
-
-        if (g.exists(libDir)) {
-            try {
-                c.execSync("npm i", {cwd: libDir, env: process.env, stdio: 'inherit'})
-                console.log(`Installed dependencies in ${libDir}`)
-            } catch (e) {
-                console.log(`Could not install dependencies in ${libDir}`)
-            }
-        } else {
-            console.log(`${libDir} not found`)
+    if (g.exists(libDir)) {
+        try {
+            c.execSync("npm i", {cwd: libDir, env: process.env, stdio: 'inherit'})
+            console.log(`Installed dependencies in ${libDir}`)
+        } catch (e) {
+            console.log(`Could not install dependencies in ${libDir}`)
         }
-    })
+    } else {
+        console.log(`${libDir} not found`)
+    }
 }
 
-exports.doInstall = doInstall;
 
-doInstall(process.argv[2])
+const lib = process.argv[2]
+
+g.ls("./apps").forEach(d => {
+
+    if (lib == null) {
+        console.log(`Installing dependencies for all libs for app ${d}`)
+        p.libs.forEach(lib => {
+            install(d, lib)
+        })
+    } else {
+        install(d, lib)
+    }
+
+})
