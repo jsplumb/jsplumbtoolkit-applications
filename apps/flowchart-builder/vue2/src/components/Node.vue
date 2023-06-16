b1cdc0474b2a4882422e220b290906f4c8fbfe8e
Simon Porritt <simon@jsplumbtoolkit.com>
2023-06-16 10:58:08 +1000

    use anchorPositions array to draw connection sources

--- apps/flowchart-builder/vue2/src/components/Node.vue
+++ apps/flowchart-builder/vue2/src/components/Node.vue
@@ -11,10 +11,15 @@
     <div :style="{width:obj.width + 'px',height:obj.height + 'px',color:obj.textColor}" class="flowchart-object" data-jtk-target="true" data-jtk-target-port-type="target">
         <span>{{obj.text}}</span>
         <jsplumb-shape :obj="obj" :shape-library="shapeLibrary"></jsplumb-shape>
-        <div class="jtk-connect jtk-connect-left" data-jtk-anchor-x="0" data-jtk-anchor-y="0.5" data-jtk-orientation-x="-1"  data-jtk-orientation-y="0" data-jtk-source="true" data-jtk-port-type="source"></div>
-        <div class="jtk-connect jtk-connect-right" data-jtk-anchor-x="1" data-jtk-anchor-y="0.5" data-jtk-orientation-x="1"  data-jtk-orientation-y="0" data-jtk-source="true" data-jtk-port-type="source"></div>
-        <div class="jtk-connect jtk-connect-top" data-jtk-anchor-x="0.5" data-jtk-anchor-y="0" data-jtk-orientation-x="0"  data-jtk-orientation-y="-1" data-jtk-source="true" data-jtk-port-type="source"></div>
-        <div class="jtk-connect jtk-connect-bottom" data-jtk-anchor-x="0.5" data-jtk-anchor-y="1" data-jtk-orientation-x="0"  data-jtk-orientation-y="1" data-jtk-source="true" data-jtk-port-type="source"></div>
+
+        <div v-for="anchor in anchorPositions"
+             :class="'jtk-connect jtk-connect-' + anchor.id"
+             :data-jtk-anchor-x="anchor.x"
+             :data-jtk-anchor-y="anchor.y"
+             :data-jtk-orientation-x="anchor.ox"
+             :data-jtk-orientation-y="anchor.oy"
+             data-jtk-source="true"
+             data-jtk-port-type="source"></div>
         <div class="node-delete node-action delete"></div>
     </div>
 </template>
