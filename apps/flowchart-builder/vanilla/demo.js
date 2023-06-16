83d0842abfe06f565bf5dbcc517e6469985c4077
Simon Porritt <simon@jsplumbtoolkit.com>
2023-06-16 10:54:20 +1000

    code docs.  change 'portId' to 'id' in anchor - we dont want it in the model, we just want to use it for CSS

--- apps/flowchart-builder/vanilla/demo.js
+++ apps/flowchart-builder/vanilla/demo.js
@@ -43,10 +43,10 @@ import {FlowchartBuilderInspector} from "./flowchart-inspector";
 initializeOrthogonalConnectorEditors()
 
 const anchorPositions = [
-    {x:0, y:0.5, ox:-1, oy:0, portId:"left"},
-    {x:1, y:0.5, ox:1, oy:0, portId:"right"},
-    {x:0.5, y:0, ox:0, oy:-1, portId:"top"},
-    {x:0.5, y:1, ox:0, oy:1, portId:"bottom"}
+    { x:0, y:0.5, ox:-1, oy:0, id:"left" },
+    { x:1, y:0.5, ox:1, oy:0, id:"right" },
+    { x:0.5, y:0, ox:0, oy:-1, id:"top" },
+    { x:0.5, y:1, ox:0, oy:1, id:"bottom" }
 ]
 
 ready(() => {
@@ -69,7 +69,7 @@ ready(() => {
         // set the Toolkit's selection mode to 'isolated', meaning it can select a set of edges, or a set of nodes, but it
         // cannot select a set of nodes and edges. In this demonstration we use an inspector that responds to events from the
         // toolkit's selection, so setting this to `isolated` helps us ensure we dont try to inspect edges and nodes at the same
-        // time. But note that we _can_ inspect multiple nodes or edges at once.
+        // time.
         selectionMode:SelectionModes.isolated,
         beforeStartConnect:(node, edgeType) => {
             return {
@@ -80,13 +80,12 @@ ready(() => {
         }
     });
 
-    window.tk = toolkit;
-
-
-    // Instruct the toolkit to render to the 'canvas' element. We pass in a view of nodes, edges and ports, which
-    // together define the look and feel and behaviour of this renderer.  Note that we can have 0 - N renderers
-    // assigned to one instance of the Toolkit..
+    // Instruct the toolkit to render to the 'canvas' element.
+    //
     renderer = toolkit.render(canvasElement, {
+        //
+        // used in the vanilla demo to extract the text color from each object and set it on its DOM element in the template
+        //
         templateMacros:{
             textColor:(data) => {
                 return data[PROPERTY_TEXT_COLOR] || DEFAULT_TEXT_COLOR
@@ -94,29 +93,37 @@ ready(() => {
         },
         view: {
             nodes: {
-                default:{
+                [DEFAULT]:{
+                    // We have a single node type, which renders a div and uses the `jtk-shape` tag to inject appropriate SVG into
+                    // the DOM element.  The `jtk-shape` tag is made available because we attach a `ShapeLibraryPalette` further down
+                    // in the code here (see https://docs.jsplumbtoolkit.com/toolkit/6.x/shape-libraries).
+                    // In this template we render a div for each value in the `anchorPositions` array, and these elements
+                    // act as connection drag sources. We use CSS to position them, but we also write out various
+                    // `data-jtk-anchor-...` properties to control their anchor positions.
                     template:`<div style="left:{{left}}px;top:{{top}}px;width:{{width}}px;height:{{height}}px;color:{{#textColor}}" class="flowchart-object flowchart-{{type}}" data-jtk-target="true" data-jtk-target-port-type="target">
                             <jtk-shape/>
                             <span>{{text}}</span> 
-                            ${anchorPositions.map(ap => `<div class="jtk-connect jtk-connect-${ap.portId}" data-jtk-port-id="${ap.portId}" data-jtk-anchor-x="${ap.x}" data-jtk-anchor-y="${ap.y}" data-jtk-orientation-x="${ap.ox}"  data-jtk-orientation-y="${ap.oy}" data-jtk-source="true" data-jtk-port-type="source"></div>`).join("\n")}
+                            ${anchorPositions.map(ap => `<div class="jtk-connect jtk-connect-${ap.id}"  data-jtk-anchor-x="${ap.x}" data-jtk-anchor-y="${ap.y}" data-jtk-orientation-x="${ap.ox}"  data-jtk-orientation-y="${ap.oy}" data-jtk-source="true"></div>`).join("\n")}
                             <div class="node-delete node-action delete"/>
                         </div>`,
-                        events: {
-                            [EVENT_TAP]: (params) => {
-                                edgeEditor.stopEditing()
-                                // if zero nodes currently selected, or the shift key wasnt pressed, make this node the only one in the selection.
-                                if (toolkit.getSelection()._nodes.length < 1 || params.e.shiftKey !== true) {
-                                    toolkit.setSelection(params.obj)
-                                } else {
-                                    // if multiple nodes already selected, or shift was pressed, add this node to the current selection.
-                                    toolkit.addToSelection(params.obj)
-                                }
+                    events: {
+                        [EVENT_TAP]: (params) => {
+                            // cancel any edge edits when the user taps a node.
+                            edgeEditor.stopEditing()
+                            // if zero nodes currently selected, or the shift key wasnt pressed, make this node the only one in the selection.
+                            if (toolkit.getSelection()._nodes.length < 1 || params.e.shiftKey !== true) {
+                                toolkit.setSelection(params.obj)
+                            } else {
+                                // if multiple nodes already selected, or shift was pressed, add this node to the current selection.
+                                toolkit.addToSelection(params.obj)
                             }
                         }
+                    }
                 }
             },
             edges: {
                 [DEFAULT]: {
+                    // Our edge uses a Blank endpoint and an Orthogonal connector.
                     endpoint:BlankEndpoint.type,
                     connector: {
                         type:OrthogonalConnector.type,
@@ -125,24 +132,31 @@ ready(() => {
                             alwaysRespectStubs:true
                         }
                     },
+                    // we set a css class on the edge and also on its label
                     cssClass:CLASS_FLOWCHART_EDGE,
                     labelClass:CLASS_EDGE_LABEL,
+                    // This says 'extract `label` from the edge data and use it as the edge's label'.
                     label:"{{label}}",
+                    // a large outlineWidth helps with selection via the mouse.
                     outlineWidth:10,
                     events: {
                         click:(p) => {
+                            // on edge click, select the edge (the inspector will update to
+                            // show this edge), and start editing it.
+                            // Note that we pass `anchorPositions` in to the edge editor to allow the user
+                            // to relocate the source/target anchors.
                             toolkit.setSelection(p.edge)
                             edgeEditor.startEditing(p.edge, {
-                                deleteButton:true
+                                deleteButton:true,
+                                anchorPositions
                             })
                         }
                     }
                 }
             },
             ports: {
-                source: {
-                    maxConnections: -1
-                },
+                // the `target` port declares that it can exist at any of the given anchorPositions, that it is
+                // a drag target, and that it can support any number of connections.
                 target: {
                     anchorPositions,
                     maxConnections: -1,
@@ -150,7 +164,9 @@ ready(() => {
                 }
             }
         },
-        // see `edge-mappings.js` for details.
+        // We declare a set of edge mappings here: a mapping from some property's value to a set of
+        // config for the edge such as overlays, css class.
+        // see https://docs.jsplumbtoolkit.com/toolkit/6.x/property-mappings and `edge-mappings.js` for details.
         propertyMappings:{
             edgeMappings:edgeMappings()
         },
@@ -158,26 +174,34 @@ ready(() => {
         layout: {
             type: AbsoluteLayout.type
         },
+        // Snap everything to a grid. This will be used for element dragging as well as resizing and also
+        // by the palette that allows users to drag new nodes on to the canvas.
         grid:{
             size:GRID_SIZE
         },
         events: {
+            // on whitespace click, clear selected node/edge and stop editing any edges.
             [EVENT_CANVAS_CLICK]: (e) => {
                 toolkit.clearSelection()
                 edgeEditor.stopEditing()
             }
         },
+        // this is mostly for dev: by default the surface will consume right clicks.
         consumeRightClick: false,
+        // a selector identifying which parts of each node should not cause the element to be dragged.
+        // typically here you'd list such things as buttons etc.
         dragOptions: {
             filter: ".jtk-draw-handle, .node-action, .node-action i"
         },
         plugins:[
+            // add a miniview plugin.
             {
                 type: MiniviewPlugin.type,
                 options: {
                     container: miniviewElement
                 }
             },
+            // this plugin allows the user to resize elements.
             {
                 type:DrawingToolsPlugin.type,
                 options:{
@@ -185,6 +209,7 @@ ready(() => {
                     heightAttribute:"height"
                 }
             },
+            // select multiple elements with a lasso
             {
                 type:LassoPlugin.type,
                 options: {
@@ -192,18 +217,14 @@ ready(() => {
                     lassoEdges:true
                 }
             },
+            // use a grid background.
             {
                 type:BackgroundPlugin.type,
                 options:GRID_BACKGROUND_OPTIONS
             }
         ],
-        magnetize:{
-            afterDrag:true
-        },
-        defaults:{
-           anchor:AnchorLocations.AutoDefault
-        },
         modelEvents:[
+            // catch the TAP event on the delete buttons inside nodes and remove the node from the model.
             {
                 event:EVENT_TAP,
                 selector:".node-delete",
@@ -213,8 +234,6 @@ ready(() => {
             }
         ]
     })
-
-    window.r = renderer
 
     // manager for editing edge paths
     edgeEditor = new EdgePathEditor(renderer, {activeMode: true})
@@ -250,7 +269,5 @@ ready(() => {
             renderer.zoomToFit()
         }
     })
-
-
 })
 
