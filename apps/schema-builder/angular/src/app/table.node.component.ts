bda7086e3bf4c1755cfa0e37d5359ead69274733
Simon Porritt <simon@jsplumbtoolkit.com>
2023-06-25 08:40:46 +1000

    some docs

--- apps/schema-builder/angular/src/app/table.node.component.ts
+++ apps/schema-builder/angular/src/app/table.node.component.ts
@@ -24,6 +24,16 @@ import {DATATYPE_VARCHAR} from "./constants"
 export class TableNodeComponent extends BaseEditableNodeComponent {
 
   addColumn():void {
+    //
+    // this is a helper method provided by the Toolkit's `BaseNodeComponent`. It will add a new port to the data
+    // for the node this component represents.  Under the hood the Toolkit does two things in response to this:
+    //
+    // 1. It uses the `portDataProperty` that we set on the `toolkitParams` in AppComponent to find the array in
+    // each node's backing data into which to write the a JS object for this port. Angular then discovers that
+    // via change detection and renders a column component for it.
+    //
+    // 2. it registers the new port data on the node in the Toolkit's data model.
+    //
     this.addNewPort("column",{
       id:uuid(),
       name:"new column",
