2ab18aef9c8ca2697ba9ae2a9e532b17129255ff
Simon Porritt <simon@jsplumbtoolkit.com>
2023-06-25 08:39:09 +1000

    docs

--- apps/schema-builder/angular/src/app/base.editable.node.ts
+++ apps/schema-builder/angular/src/app/base.editable.node.ts
@@ -1,8 +1,16 @@
 
 import {BaseNodeComponent} from "@jsplumbtoolkit/browser-ui-angular"
 
+/**
+ * Superclass for table and view components. Offers an `editName` method.
+ */
 export class BaseEditableNodeComponent extends BaseNodeComponent {
 
+  //
+  // To edit a node's name we set it as the Toolkit's selection. Our inspector (see inspector.component.ts) listens
+  // for changes to the selection and will display an inspector for this node, in which the user can edit the
+  // node's properties.
+  //
   editName():void {
     this.toolkit.setSelection(this.getNode())
   }
