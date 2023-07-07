e463cf2421bead114bd7b3352c67699320fbc9d9
Simon Porritt <simon@jsplumbtoolkit.com>
2023-07-07 15:15:33 +1000

    wire up the node delete button

--- apps/flowchart-builder/react/src/NodeComponent.js
+++ apps/flowchart-builder/react/src/NodeComponent.js
@@ -4,7 +4,7 @@ import { ShapeComponent } from "@jsplumbtoolkit/browser-ui-react"
 
 export default function NodeComponent({ctx, shapeLibrary}) {
 
-    const { vertex } = ctx;
+    const { vertex, toolkit } = ctx;
     const data = vertex.data;
 
     return <div style={{width:data.width + 'px',height:data.height + 'px',color:data.textColor}} className="flowchart-object" data-jtk-target="true" data-jtk-target-port-type="target">
@@ -14,6 +14,6 @@ export default function NodeComponent({ctx, shapeLibrary}) {
         <div className="jtk-connect jtk-connect-right" data-jtk-anchor-x="1" data-jtk-anchor-y="0.5" data-jtk-orientation-x="1"  data-jtk-orientation-y="0" data-jtk-source="true" data-jtk-port-type="source"></div>
         <div className="jtk-connect jtk-connect-top" data-jtk-anchor-x="0.5" data-jtk-anchor-y="0" data-jtk-orientation-x="0"  data-jtk-orientation-y="-1" data-jtk-source="true" data-jtk-port-type="source"></div>
         <div className="jtk-connect jtk-connect-bottom" data-jtk-anchor-x="0.5" data-jtk-anchor-y="1" data-jtk-orientation-x="0"  data-jtk-orientation-y="1" data-jtk-source="true" data-jtk-port-type="source"></div>
-        <div className="node-delete node-action delete"></div>
+        <div className="node-delete node-action delete" onClick={() => toolkit.removeNode(vertex)}></div>
     </div>
 }
