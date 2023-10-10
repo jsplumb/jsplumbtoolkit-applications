da57d59610a8bd95d279b9972d6b762232c63205
Simon Porritt <simon@jsplumbtoolkit.com>
2023-10-10 10:40:55 +0100

    add react orgchart

---
+++ apps/orgchart/react/README.md
@@ -0,0 +1,3 @@
+## Orgchart
+
+Built with the Toolkit's React integration. This app can be used as a foundation for a React orgchart.
---
+++ apps/orgchart/react/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import OrgchartComponent from "./OrgchartComponent";
+
+function App() {
+  return (
+    <div className="App">
+      <OrgchartComponent/>
+    </div>
+  );
+}
+
+export default App;
