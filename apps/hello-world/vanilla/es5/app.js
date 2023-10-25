ea2595c89296bf6775aebeab92b47dc0bde71933
Simon Porritt <simon@jsplumbtoolkit.com>
2023-10-25 09:31:28 +0100

    added es5, es6, typescript hello worlds

---
+++ apps/hello-world/vanilla/es5/app.js
@@ -0,0 +1,83 @@
+
+jsPlumbToolkit.ready(function() {
+
+    // Get a new Toolkit instance
+    const toolkit = jsPlumbToolkit.newInstance()
+
+    // Get the DOM element to render into
+    const container = document.getElementById("container")
+
+    // Render to a Surface.
+    const surface = toolkit.render(container, {
+        layout:{
+            // there are several layouts that ship with the toolkit.
+            type:jsPlumbToolkit.AbsoluteLayout.type,
+            options:{
+                //... if your chosen layout is configurable, options go here
+            }
+        },
+        // Allows us to specify edge color (and line width) in each edge's backing data
+        simpleEdgeStyles:true,
+        // Use a Continuous anchor and a blank endpoint by default.
+        defaults:{
+            anchor:jsPlumbToolkit.AnchorLocations.Continuous,
+            endpoint:jsPlumbToolkit.BlankEndpoint.type
+        },
+        events:{
+            "canvasClick":function() {
+                toolkit.clearSelection()
+            }
+        },
+        // map node types and edge types to appearance and behaviour
+        view:{
+            nodes:{
+                // abstract parent node definition - declares a tap listener
+                clickable:{
+                    events:{
+                        tap:(p) => {
+                            alert(`You clicked on node ${p.obj.id}`)
+                            toolkit.setSelection(p.obj)
+                        }
+                    }
+                },
+                // definition for 'hello' nodes. Extends 'clickable' to get the tap listener.
+                hello:{
+                    parent:"clickable",
+                    template:'<div class="hello-node">{{label}}</div>'
+                },
+                // definition for 'world' nodes. Extends 'clickable' to get the tap listener.
+                world:{
+                    parent:"clickable",
+                    template:'<div class="world-node">{{label}}</div>'
+                }
+            },
+            edges:{
+                // a default edge definition. Declares an arrow overlay at its tip and extracts 'label' from
+                // edge data and displays it as a label overlay (by default at location 0.5)
+                [jsPlumbToolkit.DEFAULT]:{
+                    overlays:[
+                        {
+                            type:jsPlumbToolkit.ArrowOverlay.type,
+                            options:{
+                                location: 1
+                            }
+                        }
+                    ],
+                    label:"{{label}}"
+                }
+            }
+        }
+    })
+
+    toolkit.load({
+        data:{
+            nodes:[
+                { id:"1", type:"hello", label:"Hello", left:50, top:50 },
+                { id:"2", type:"world", label:"World", left:350, top:50 }
+            ],
+            edges:[
+                { source:"1", target:"2", data:{label:"a label", color:"purple"} }
+            ]
+        }
+    })
+})
